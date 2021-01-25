801599e23517b6a2b0f3d5251128d6e1c28977eb
lordrichard6 <paulolopesreizinho@gmail.com>
2021-01-25 07:33:29 +0100

    search widget with useState and useEffect defined

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front-end javascript framework'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favourite JS library among engineers'
-    },
-    {
-        title: 'How do you use React?',
-        content: 'You use React by creating components'
-    }
-]
+// const items = [
+//     {
+//         title: 'What is React?',
+//         content: 'React is a front-end javascript framework'
+//     },
+//     {
+//         title: 'Why use React?',
+//         content: 'React is a favourite JS library among engineers'
+//     },
+//     {
+//         title: 'How do you use React?',
+//         content: 'You use React by creating components'
+//     }
+// ]
 
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
-    </div>  
+      {/* <Accordion items={items} /> */}
+      <Search />
+    </div>
   );
 };
---
+++ src/components/Search.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data);
+    };
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search</label>
+          <input
+            type="text"
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
